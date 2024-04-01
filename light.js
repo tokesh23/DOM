9edc922cee37c20b8ded5ba47e1c8e176e6ba1e9
tokesh23 <tokesh1998@gmail.com>
2024-04-01 10:19:32 +0530

    This is my dom project

---
+++ light.js
@@ -0,0 +1,4 @@
+ function bulbon(){
+    let bulboj=document.querySelector(".bulboff")
+    bulboj.setAttribute("src","Amazon Clone/pic_bulboff.gif")
+ }
---
+++ script.js
@@ -0,0 +1,44 @@
+// let newBtn = document.createElement("button")
+// document.body.prepend(newBtn)
+// newBtn.innerText = "click me"
+// newBtn.style.background = "red"
+// newBtn.style.color = "white"
+
+// let newBtn = document.createElement("button")
+// // document.body.append(newBtn)
+// newBtn.innerText = "click me"
+// newBtn.style.background = "yellow"
+// newBtn.style.color= "green"
+// newBtn.style.width= "70px"
+// newBtn.style.height="50px"
+// newBtn.style.border ="5px solid black"
+
+
+// document.body.childNodes[3].style.background = "pink"
+
+// let list = document.getElementById("list")
+// list.before(newBtn)
+
+
+// let title = document.getElementById("title2")
+// title.after(newBtn)
+
+
+let h1= document.getElementById("heading")
+
+// console.log(h1.getAttribute("id"));
+// h1.setAttribute("class", "head")
+
+function happen() {
+    console.log("Hii there");
+}
+
+h1.addEventListener("click", happen)
+
+let title2=document.getElementById("heading")
+function happen(){
+    console.log("any one")
+
+}
+ 
+
